e1cb4e005b497d1a80c4afad024626410b9c6c67
Glêsio Santos <glesioss@gmail.com>
2024-10-20 19:50:57 -0300

    refatorando o armazenamento de valores chutados

--- advinha/maior_ou_menor.rb
+++ advinha/maior_ou_menor.rb
@@ -1,30 +1,3 @@
-da_boas_vinda
-numero_secreto = sorteia_numero_secreto
-
-total_de_tentativas = 3
-
-for tentativa in 1..total_de_tentativas
-    puts "\n\n\n"
-    puts "Tentativa #{tentativa} de #{total_de_tentativas}"
-    puts "Entre com o número"
-    chute = gets
-    puts "Será que acertou? Você chutou #{chute}"
-    acertou = numero_secreto == chute.to_i
-    maior = numero_secreto > chute.to_i
-    if acertou
-        puts "Acertou!"
-        break
-    else 
-        if maior
-        puts "O número secreto é maior"
-        else  
-            puts "O número secreto é menor"
-        end    
-    end
-end    
-
-
-
 # functions
 def da_boas_vinda
     puts "Bem vindo ao jogo de advinhação"
@@ -41,3 +14,46 @@ def sorteia_numero_secreto
     puts "Escolhido .... que tal adivinha hoje nosso número secreto?"
     return numero_secreto
 end
+
+def pede_um_numero(chutes, tentativa, total_tentativas)
+    puts "\n\n\n"
+    puts "Tentativa #{tentativa} de #{total_tentativas}"
+    puts "Chutes até agora: "+chutes.to_s
+    puts "Entre com o número"
+    chute = gets
+    puts "Será que acertou? Você chutou #{chute}"
+    chute.to_i
+end
+
+def verificar_se_acertou(numero_secreto, chute)
+    acertou = numero_secreto == chute.to_i
+    if acertou
+        puts "Acertou!"
+        return true
+    else 
+        maior = numero_secreto > chute.to_i
+        if maior
+            puts "O número secreto é maior"
+        else  
+            puts "O número secreto é menor"
+        end    
+    end
+
+    false
+end
+
+da_boas_vinda
+numero_secreto = sorteia_numero_secreto
+limite_de_tentativas = 3
+chutes = []
+
+for tentativa in 1..limite_de_tentativas
+    # total_de_chutes = 0
+    chute = pede_um_numero chutes, tentativa, limite_de_tentativas
+    # chutes[chutes.size] = chute
+    # total_de_chutes += 1
+    chutes << chute
+    if verificar_se_acertou numero_secreto, chute
+        break 
+    end
+end    
