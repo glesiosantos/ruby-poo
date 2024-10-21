798cac0093335e95d18fea79cc1f634e9e160333
Glêsio Santos <glesioss@gmail.com>
2024-10-21 09:22:10 -0300

    adiciona rand para gerar numero sorteado aleatoriamente

--- advinha/maior_ou_menor.rb
+++ advinha/maior_ou_menor.rb
@@ -5,23 +5,23 @@ def da_boas_vinda
     nome = gets
     puts "\n\n\n"
     puts "Começaremos o jogo para você, #{nome}"
-    puts "\n\n\n"
+    puts "\n"
 end
 
 def sorteia_numero_secreto
     puts "Escolhendo um número secreto entre 0 e 200 ...."
-    numero_secreto = 175
+    sorteado = rand(200)
     puts "Escolhido .... que tal adivinha hoje nosso número secreto?"
-    return numero_secreto
+    return sorteado
 end
 
 def pede_um_numero(chutes, tentativa, total_tentativas)
-    puts "\n\n\n"
+    puts "\n"
     puts "Tentativa #{tentativa} de #{total_tentativas}"
     puts "Chutes até agora: "+chutes.to_s
     puts "Entre com o número"
     chute = gets
-    puts "Será que acertou? Você chutou #{chute}"
+    # puts "Será que acertou? Você chutou #{chute}"
     chute.to_i
 end
 
@@ -46,14 +46,21 @@ da_boas_vinda
 numero_secreto = sorteia_numero_secreto
 limite_de_tentativas = 3
 chutes = []
+pontos_ate_agora = 1000
 
 for tentativa in 1..limite_de_tentativas
-    # total_de_chutes = 0
     chute = pede_um_numero chutes, tentativa, limite_de_tentativas
-    # chutes[chutes.size] = chute
-    # total_de_chutes += 1
     chutes << chute
+    pontos_a_perder = (chute - numero_secreto) * 0.5
+    pontos_a_perder = -pontos_a_perder if pontos_a_perder < 0
+    pontos_ate_agora -= pontos_a_perder / 2.0
+
     if verificar_se_acertou numero_secreto, chute
         break 
     end
 end    
+
+puts "\n\n\n"
+puts "Sua tentativas #{chutes}"
+puts "O número sorteado era #{numero_secreto}"
+puts "Você ganhou  #{pontos_ate_agora} pontos."
